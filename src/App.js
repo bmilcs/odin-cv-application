d0fdf5a7f9ee7000b3f7453d52b1b9574bc7eae2
Bryan Miller <bmilcs@yahoo.com>
2022-12-22 17:09:50 -0500

    Move state handling & eventhandlers to App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,100 @@ import React from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
-
-// section: name, email, phone
-// section: education, school name, study title, date of study
-// section: experience, company name, position title, main job tasks, date from-until
+import {
+  emptyForm,
+  EducationItem,
+  WorkItem,
+} from "./components/form/emptyForm";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...emptyForm };
+  }
+
+  handlePersonalChange = (e) => {
+    const { id, value } = e.target;
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        personal: {
+          ...prevState.personal,
+          [id]: value,
+        },
+      };
+    });
+  };
+
+  handleWorkEducationChange = (e) => {
+    // this.state.education & this.state.work contain arrays of objects
+    // e.target (ie: an input element)'s html id contains the following info:
+    // category (education or work), the corresponding state object key(school, degree, startDate, etc.)
+    // and an uniqid (work or school id).
+    // - example: "education-degree-sj208alq"
+
+    const { id, value } = e.target;
+    const categoryObjKeyAndUniqID = id.split("-");
+    const category = categoryObjKeyAndUniqID[0];
+    const objKey = categoryObjKeyAndUniqID[1];
+    const uniqID = categoryObjKeyAndUniqID[2];
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        [category]: prevState[category].map((item) => {
+          if (item.id !== uniqID) return item;
+          return {
+            ...item,
+            [objKey]: value,
+          };
+        }),
+      };
+    });
+  };
+
+  handleAddItem = (e) => {
+    e.preventDefault();
+
+    const { id } = e.target;
+    const category = id.split("-")[1];
+    const newObj = category === "education" ? EducationItem() : WorkItem();
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        [category]: [...prevState[category], newObj],
+      };
+    });
+  };
+
+  handleDeleteItem = (e) => {
+    e.preventDefault();
+
+    const { id } = e.target;
+    const categoryAndUniqID = id.split("-");
+    const category = categoryAndUniqID[1];
+    const uniqid = categoryAndUniqID[2];
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        [category]: prevState[category].filter((item) => item.id !== uniqid),
+      };
+    });
+  };
+
   render() {
     return (
       <div className="app-container">
         <Header />
-        <Form />
+        <Form
+          state={this.state}
+          onDelete={this.handleDeleteItem}
+          onAdd={this.handleAddItem}
+          onPersonalChange={this.handlePersonalChange}
+          onWorkEducationChange={this.handleWorkEducationChange}
+        />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,103 +2,26 @@ import React from "react";
 import Personal from "./form/Personal";
 import Education from "./form/Education";
 import Work from "./form/Work";
-import { emptyForm, EducationItem, WorkItem } from "./form/emptyForm";
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...emptyForm };
-  }
-
-  handlePersonalChange = (e) => {
-    const { id, value } = e.target;
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        personal: {
-          ...prevState.personal,
-          [id]: value,
-        },
-      };
-    });
-  };
-
-  handleWorkEducationChange = (e) => {
-    // this.state.education & this.state.work contain arrays of objects
-    // e.target (ie: an input element)'s html id contains the following info:
-    // category (education or work), the corresponding state object key(school, degree, startDate, etc.)
-    // and an uniqid (work or school id).
-    // - example: "education-degree-sj208alq"
-
-    const { id, value } = e.target;
-    const categoryObjKeyAndUniqID = id.split("-");
-    const category = categoryObjKeyAndUniqID[0];
-    const objKey = categoryObjKeyAndUniqID[1];
-    const uniqID = categoryObjKeyAndUniqID[2];
-
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        [category]: prevState[category].map((item) => {
-          if (item.id !== uniqID) return item;
-          return {
-            ...item,
-            [objKey]: value,
-          };
-        }),
-      };
-    });
-  };
-
-  handleAddItem = (e) => {
-    e.preventDefault();
-
-    const { id } = e.target;
-    const category = id.split("-")[1];
-    const newObj = category === "education" ? EducationItem() : WorkItem();
-
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        [category]: [...prevState[category], newObj],
-      };
-    });
-  };
-
-  handleDeleteItem = (e) => {
-    e.preventDefault();
-
-    const { id } = e.target;
-    const categoryAndUniqID = id.split("-");
-    const category = categoryAndUniqID[1];
-    const uniqid = categoryAndUniqID[2];
-
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        [category]: prevState[category].filter((item) => item.id !== uniqid),
-      };
-    });
-  };
-
   render() {
+    const { state, onDelete, onAdd, onPersonalChange, onWorkEducationChange } =
+      this.props;
+
     return (
       <form>
-        <Personal
-          state={this.state.personal}
-          onChange={this.handlePersonalChange}
-        />
+        <Personal state={state.personal} onChange={onPersonalChange} />
         <Education
-          state={this.state.education}
-          onChange={this.handleWorkEducationChange}
-          onAdd={this.handleAddItem}
-          onDelete={this.handleDeleteItem}
+          state={state.education}
+          onChange={onWorkEducationChange}
+          onAdd={onAdd}
+          onDelete={onDelete}
         />
         <Work
-          state={this.state.work}
-          onChange={this.handleWorkEducationChange}
-          onAdd={this.handleAddItem}
-          onDelete={this.handleDeleteItem}
+          state={state.work}
+          onChange={onWorkEducationChange}
+          onAdd={onAdd}
+          onDelete={onDelete}
         />
       </form>
     );
