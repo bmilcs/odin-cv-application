84f217487709007dc0a251e96d2dcd5d6414ba6c
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:31:23 -0500

    Refactor Preview to be a functional component & end/start date conditions

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,69 +1,79 @@
 import React from "react";
 import "../styles/Preview.css";
 
-class Preview extends React.Component {
-  render() {
-    const { state, clearAllFieldsHandler, loadSampleDataHandler } = this.props;
-    const { personal, education, work } = state;
+const Preview = (props) => {
+  const { state, clearAllFieldsHandler, loadSampleDataHandler } = props;
+  const { personal, education, work } = state;
 
-    const loadSampleDataButton = (
-      <button className="standard-button" onClick={loadSampleDataHandler}>
-        Load Sample Data
-      </button>
-    );
+  const loadSampleDataButton = (
+    <button className="standard-button" onClick={loadSampleDataHandler}>
+      Load Sample Data
+    </button>
+  );
 
-    const clearAllFieldsButton = (
-      <button className="standard-button" onClick={clearAllFieldsHandler}>
-        Clear All Fields
-      </button>
-    );
+  const clearAllFieldsButton = (
+    <button className="standard-button" onClick={clearAllFieldsHandler}>
+      Clear All Fields
+    </button>
+  );
 
-    return (
-      <div className="left-column">
-        <div className="preview-container">
-          <div className="preview-header-container">
-            <h1 className="preview-name">
-              {personal.firstName} {personal.lastName}
-            </h1>
-            <p>{personal.phone}</p>
-            <p>{personal.email}</p>
-          </div>
-          <div className="preview-content-container">
-            <h2 className="preview-section-title">Education</h2>
-            {education.map((edu) => {
-              return (
-                <div className="preview-education-container" key={edu.degree}>
-                  <h3>{edu.school}</h3>
-                  <p>{edu.degree}</p>
+  return (
+    <div className="left-column">
+      <div className="preview-container">
+        <div className="preview-header-container">
+          <h1 className="preview-name">
+            {personal.firstName} {personal.lastName}
+          </h1>
+          <p>{personal.phone}</p>
+          <p>{personal.email}</p>
+        </div>
+        <div className="preview-content-container">
+          <h2 className="preview-section-title">Education</h2>
+          {education.map((edu) => {
+            return (
+              <div className="preview-education-container" key={edu.degree}>
+                <h3>{edu.school}</h3>
+                <p>{edu.degree}</p>
+                {edu.startDate && edu.endDate ? (
                   <p>
                     {edu.startDate} - {edu.endDate}
                   </p>
-                </div>
-              );
-            })}
-            <h2 className="preview-section-title">Work </h2>
-            {work.map((w) => {
-              return (
-                <div className="preview-work-container" key={w.employer}>
-                  <h3>{w.title}</h3>
-                  <p>{w.employer}</p>
-                  {w.startDate && w.endDate ? (
-                    <p>
-                      {w.startDate} - {w.endDate}
-                    </p>
-                  ) : null}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="preview-button-container">
-          {loadSampleDataButton}
-          {clearAllFieldsButton}
+                ) : edu.startDate || edu.endDate ? (
+                  <p>
+                    {edu.startDate}
+                    {edu.endDate}
+                  </p>
+                ) : null}
+              </div>
+            );
+          })}
+          <h2 className="preview-section-title">Work </h2>
+          {work.map((w) => {
+            return (
+              <div className="preview-work-container" key={w.employer}>
+                <h3>{w.title}</h3>
+                <p>{w.employer}</p>
+                {w.startDate && w.endDate ? (
+                  <p>
+                    {w.startDate} - {w.endDate}
+                  </p>
+                ) : w.startDate || w.endDate ? (
+                  <p>
+                    {w.startDate}
+                    {w.endDate}
+                  </p>
+                ) : null}
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-  }
-}
+      <div className="preview-button-container">
+        {loadSampleDataButton}
+        {clearAllFieldsButton}
+      </div>
+    </div>
+  );
+};
 
 export default Preview;
