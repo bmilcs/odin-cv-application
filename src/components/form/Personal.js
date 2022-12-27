38bfc236a29cac8357403d01ef64b7c70f236ef1
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:42:35 -0500

    Refactor Personal to be a functional component

--- src/components/form/Personal.js
+++ src/components/form/Personal.js
@@ -3,49 +3,47 @@ import FormSection from "../utils/FormSection";
 import InputGroup from "../utils/InputGroup";
 import "../../styles/FormSection.css";
 
-class Personal extends React.Component {
-  render() {
-    const { state, onChange } = this.props;
-    const { firstName, lastName, email, phone } = state;
-    const fields = [
-      {
-        description: "First Name",
-        id: "firstName",
-        classes: "",
-        type: "text",
-        value: firstName,
-      },
-      {
-        description: "Last Name",
-        id: "lastName",
-        classes: "",
-        type: "text",
-        value: lastName,
-      },
-      {
-        description: "Phone",
-        id: "phone",
-        classes: "",
-        type: "tel",
-        value: phone,
-      },
-      {
-        description: "E-mail",
-        id: "email",
-        classes: "",
-        type: "email",
-        value: email,
-      },
-    ];
+const Personal = (props) => {
+  const { state, onChange } = props;
+  const { firstName, lastName, email, phone } = state;
+  const fields = [
+    {
+      description: "First Name",
+      id: "firstName",
+      classes: "",
+      type: "text",
+      value: firstName,
+    },
+    {
+      description: "Last Name",
+      id: "lastName",
+      classes: "",
+      type: "text",
+      value: lastName,
+    },
+    {
+      description: "Phone",
+      id: "phone",
+      classes: "",
+      type: "tel",
+      value: phone,
+    },
+    {
+      description: "E-mail",
+      id: "email",
+      classes: "",
+      type: "email",
+      value: email,
+    },
+  ];
 
-    return (
-      <FormSection title="Personal Info">
-        <div className="personal-info card">
-          <InputGroup fields={fields} onChange={onChange} />
-        </div>
-      </FormSection>
-    );
-  }
-}
+  return (
+    <FormSection title="Personal Info">
+      <div className="personal-info card">
+        <InputGroup fields={fields} onChange={onChange} />
+      </div>
+    </FormSection>
+  );
+};
 
 export default Personal;
