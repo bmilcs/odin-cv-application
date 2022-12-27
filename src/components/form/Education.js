589fcfd95024e25efd2e20736d66aa5232e1a7d2
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:36:04 -0500

    Refactor Education to be a functional component

--- src/components/form/Education.js
+++ src/components/form/Education.js
@@ -6,83 +6,81 @@ import DeleteIcon from "../assets/DeleteIcon";
 import "../../styles/FormSection.css";
 import AddThickIcon from "../assets/AddThickIcon";
 
-class Education extends React.Component {
-  render() {
-    const { state, onChange, onAdd, onDelete } = this.props;
+const Education = (props) => {
+  const { state, onChange, onAdd, onDelete } = props;
 
-    const educationGroups = state.map((educationItem) => {
-      const { id, school, degree, startDate, endDate } = educationItem;
-      return {
-        id: id,
-        fields: [
-          {
-            description: "School",
-            id: `education-school-${id}`,
-            classes: "",
-            type: "text",
-            value: school,
-          },
-          {
-            description: "Degree",
-            id: `education-degree-${id}`,
-            classes: "",
-            type: "text",
-            value: degree,
-          },
-          {
-            description: "Start Date",
-            id: `education-startDate-${id}`,
-            classes: "",
-            type: "text",
-            value: startDate,
-          },
-          {
-            description: "End Date",
-            id: `education-endDate-${id}`,
-            classes: "",
-            type: "text",
-            value: endDate,
-          },
-        ],
-      };
-    });
+  const educationGroups = state.map((educationItem) => {
+    const { id, school, degree, startDate, endDate } = educationItem;
+    return {
+      id: id,
+      fields: [
+        {
+          description: "School",
+          id: `education-school-${id}`,
+          classes: "",
+          type: "text",
+          value: school,
+        },
+        {
+          description: "Degree",
+          id: `education-degree-${id}`,
+          classes: "",
+          type: "text",
+          value: degree,
+        },
+        {
+          description: "Start Date",
+          id: `education-startDate-${id}`,
+          classes: "",
+          type: "text",
+          value: startDate,
+        },
+        {
+          description: "End Date",
+          id: `education-endDate-${id}`,
+          classes: "",
+          type: "text",
+          value: endDate,
+        },
+      ],
+    };
+  });
 
-    const addEducationButton = (
-      <button id="add-education-btn" onClick={onAdd} className="add-button">
-        <AddThickIcon />
-      </button>
-    );
+  const addEducationButton = (
+    <button id="add-education-btn" onClick={onAdd} className="add-button">
+      <AddThickIcon />
+    </button>
+  );
 
-    return (
-      <FormSection title="Education" addButton={addEducationButton}>
-        {educationGroups.map((eduGroup) => (
-          <div className="education-item card" key={`edu-group-${eduGroup.id}`}>
-            <InputGroup
-              fields={eduGroup.fields}
-              key={`edu-${eduGroup.id}`}
-              onChange={onChange}
-            />
-            <div className="button-container">
-              <button
-                id="add-education-btn"
-                onClick={onAdd}
-                className="add-button"
-              >
-                <AddIcon />
-              </button>
-              <button
-                id={`delete-education-${eduGroup.id}`}
-                onClick={onDelete}
-                className="delete-button"
-              >
-                <DeleteIcon />
-              </button>
-            </div>
+  return (
+    <FormSection title="Education" addButton={addEducationButton}>
+      {educationGroups.map((eduGroup) => (
+        <div className="education-item card" key={`edu-group-${eduGroup.id}`}>
+          <InputGroup
+            fields={eduGroup.fields}
+            key={`edu-${eduGroup.id}`}
+            onChange={onChange}
+          />
+          <div className="button-container">
+            <button
+              id="add-education-btn"
+              onClick={onAdd}
+              className="add-button"
+            >
+              <AddIcon />
+            </button>
+            <button
+              id={`delete-education-${eduGroup.id}`}
+              onClick={onDelete}
+              className="delete-button"
+            >
+              <DeleteIcon />
+            </button>
           </div>
-        ))}
-      </FormSection>
-    );
-  }
-}
+        </div>
+      ))}
+    </FormSection>
+  );
+};
 
 export default Education;
