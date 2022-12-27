627da91c36efae488161dcbbab6b790f78b8d12d
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:44:38 -0500

    Refactor InputGroup to be a functional component

--- src/components/utils/InputGroup.js
+++ src/components/utils/InputGroup.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Input from "./Input";
 
-class InputGroup extends React.Component {
-  render() {
-    const { fields, onChange, children } = this.props;
-    return (
-      <div className="input-group">
-        {fields.map((field) => {
-          const { description, id, type, value, classes } = field;
-          return (
-            <Input
-              description={description}
-              id={id}
-              classes={classes}
-              key={id}
-              type={type}
-              value={value}
-              onChangeHandler={onChange}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const InputGroup = (props) => {
+  const { fields, onChange, children } = props;
+  return (
+    <div className="input-group">
+      {fields.map((field) => {
+        const { description, id, type, value, classes } = field;
+        return (
+          <Input
+            description={description}
+            id={id}
+            classes={classes}
+            key={id}
+            type={type}
+            value={value}
+            onChangeHandler={onChange}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default InputGroup;
