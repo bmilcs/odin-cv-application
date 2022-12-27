0613293b7332c8ceda958920114e97f6b5291ad5
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:44:13 -0500

    Refactor Input to be a functional component

--- src/components/utils/Input.js
+++ src/components/utils/Input.js
@@ -1,25 +1,22 @@
 import React from "react";
 import "../../styles/TextFields.css";
 
-class Input extends React.Component {
-  render() {
-    const { description, id, classes, type, value, onChangeHandler } =
-      this.props;
-    return (
-      <div className="input-container">
-        <label htmlFor={id}>{description}</label>
-        <input
-          type={type}
-          name={id}
-          className={classes}
-          key={id}
-          id={id}
-          value={value}
-          onChange={onChangeHandler}
-        ></input>
-      </div>
-    );
-  }
-}
+const Input = (props) => {
+  const { description, id, classes, type, value, onChangeHandler } = props;
+  return (
+    <div className="input-container">
+      <label htmlFor={id}>{description}</label>
+      <input
+        type={type}
+        name={id}
+        className={classes}
+        key={id}
+        id={id}
+        value={value}
+        onChange={onChangeHandler}
+      ></input>
+    </div>
+  );
+};
 
 export default Input;
