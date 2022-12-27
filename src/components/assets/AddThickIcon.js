9787cbaeefe8582202b5c793665c0be190c9c823
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:46:31 -0500

    Refactor assets/. to be functional components

--- src/components/assets/AddThickIcon.js
+++ src/components/assets/AddThickIcon.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class AddThickIcon extends React.Component {
-  render() {
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40">
-        <path d="M17.792 32.458v-10.25H7.5v-4.416h10.292V7.5h4.416v10.292H32.5v4.416H22.208v10.25Z" />
-      </svg>
-    );
-  }
-}
+const AddThickIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40">
+      <path d="M17.792 32.458v-10.25H7.5v-4.416h10.292V7.5h4.416v10.292H32.5v4.416H22.208v10.25Z" />
+    </svg>
+  );
+};
 
 export default AddThickIcon;
