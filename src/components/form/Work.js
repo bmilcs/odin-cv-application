22930fa775eae252b28c4e0d4583db4b5819242a
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:42:51 -0500

    Refactor Education to be a functional component

--- src/components/form/Work.js
+++ src/components/form/Work.js
@@ -6,87 +6,85 @@ import AddThickIcon from "../assets/AddThickIcon";
 import DeleteIcon from "../assets/DeleteIcon";
 import "../../styles/FormSection.css";
 
-class Work extends React.Component {
-  render() {
-    const { state, onChange, onAdd, onDelete } = this.props;
+const Work = (props) => {
+  const { state, onChange, onAdd, onDelete } = props;
 
-    const workGroups = state.map((workItem) => {
-      const { id, employer, title, responsibilities, startDate, endDate } =
-        workItem;
-      return {
-        id: id,
-        fields: [
-          {
-            description: "Employer",
-            id: `work-employer-${id}`,
-            classes: "",
-            type: "text",
-            value: employer,
-          },
-          {
-            description: "Job Title",
-            id: `work-title-${id}`,
-            classes: "",
-            type: "text",
-            value: title,
-          },
-          {
-            description: "Responsibilities",
-            id: `work-responsibilities-${id}`,
-            classes: "",
-            type: "text",
-            value: responsibilities,
-          },
-          {
-            description: "Start Date",
-            id: `work-startDate-${id}`,
-            classes: "",
-            type: "text",
-            value: startDate,
-          },
-          {
-            description: "End Date",
-            id: `work-endDate-${id}`,
-            classes: "",
-            type: "text",
-            value: endDate,
-          },
-        ],
-      };
-    });
+  const workGroups = state.map((workItem) => {
+    const { id, employer, title, responsibilities, startDate, endDate } =
+      workItem;
+    return {
+      id: id,
+      fields: [
+        {
+          description: "Employer",
+          id: `work-employer-${id}`,
+          classes: "",
+          type: "text",
+          value: employer,
+        },
+        {
+          description: "Job Title",
+          id: `work-title-${id}`,
+          classes: "",
+          type: "text",
+          value: title,
+        },
+        {
+          description: "Responsibilities",
+          id: `work-responsibilities-${id}`,
+          classes: "",
+          type: "text",
+          value: responsibilities,
+        },
+        {
+          description: "Start Date",
+          id: `work-startDate-${id}`,
+          classes: "",
+          type: "text",
+          value: startDate,
+        },
+        {
+          description: "End Date",
+          id: `work-endDate-${id}`,
+          classes: "",
+          type: "text",
+          value: endDate,
+        },
+      ],
+    };
+  });
 
-    const addWorkButton = (
-      <button id="add-work-btn" onClick={onAdd} className="add-button">
-        <AddThickIcon className="icon" />
-      </button>
-    );
+  const addWorkButton = (
+    <button id="add-work-btn" onClick={onAdd} className="add-button">
+      <AddThickIcon className="icon" />
+    </button>
+  );
 
-    return (
-      <FormSection title="Work Experience" addButton={addWorkButton}>
-        {workGroups.map((workGroup) => (
-          <div className="work-item card" key={`work-group-${workGroup.id}`}>
-            <InputGroup
-              fields={workGroup.fields}
-              key={`edu-${workGroup.id}`}
-              onChange={onChange}
-            />
-            <div className="button-container">
-              <button id="add-work-btn" onClick={onAdd} className="add-button">
-                <AddIcon className="icon" />
-              </button>
-              <button
-                id={`delete-work-${workGroup.id}`}
-                onClick={onDelete}
-                className="delete-button"
-              >
-                <DeleteIcon />
-              </button>
-            </div>
+  return (
+    <FormSection title="Work Experience" addButton={addWorkButton}>
+      {workGroups.map((workGroup) => (
+        <div className="work-item card" key={`work-group-${workGroup.id}`}>
+          <InputGroup
+            fields={workGroup.fields}
+            key={`edu-${workGroup.id}`}
+            onChange={onChange}
+          />
+          <div className="button-container">
+            <button id="add-work-btn" onClick={onAdd} className="add-button">
+              <AddIcon className="icon" />
+            </button>
+            <button
+              id={`delete-work-${workGroup.id}`}
+              onClick={onDelete}
+              className="delete-button"
+            >
+              <DeleteIcon />
+            </button>
           </div>
-        ))}
-      </FormSection>
-    );
-  }
-}
+        </div>
+      ))}
+    </FormSection>
+  );
+};
 
 export default Work;
