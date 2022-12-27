e87793c638f8c024929154f9fc92cbd27fa50546
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:32:23 -0500

    Refactor Header to be a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,20 @@ import React from "react";
 import HeaderLogo from "./assets/HeaderLogo";
 import "../styles/Header.css";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <div className="header-wrapper">
-          <HeaderLogo />
-          <div className="header-text-container">
-            <h1 className="header">
-              CV<span> Application</span>
-            </h1>
-            <p>A React-Based Curriculum Vitae Application Creator</p>
-          </div>
+const Header = () => {
+  return (
+    <header>
+      <div className="header-wrapper">
+        <HeaderLogo />
+        <div className="header-text-container">
+          <h1 className="header">
+            CV<span> Application</span>
+          </h1>
+          <p>A React-Based Curriculum Vitae Application Creator</p>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
