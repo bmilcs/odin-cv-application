9df4cc180ecbaafabc5086991538b40299df66b1
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:34:47 -0500

    Refactor Footer to be a functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,16 @@ import React from "react";
 import "../styles/Footer.css";
 import GitHubIcon from "./assets/GitHubIcon";
 
-export class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <a href="https://github.com/bmilcs/odin-cv-application">
-          bmilcs
-          <GitHubIcon />
-          2022
-        </a>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/bmilcs/odin-cv-application">
+        bmilcs
+        <GitHubIcon />
+        2022
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
