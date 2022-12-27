8eb301274d00d3930afe111fc627d25bf496975f
Bryan Miller <bmilcs@yahoo.com>
2022-12-27 17:33:54 -0500

    Refactor Form to be a functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,29 +3,27 @@ import Personal from "./form/Personal";
 import Education from "./form/Education";
 import Work from "./form/Work";
 
-class Form extends React.Component {
-  render() {
-    const { state, onDelete, onAdd, onPersonalChange, onWorkEducationChange } =
-      this.props;
+const Form = (props) => {
+  const { state, onDelete, onAdd, onPersonalChange, onWorkEducationChange } =
+    props;
 
-    return (
-      <form>
-        <Personal state={state.personal} onChange={onPersonalChange} />
-        <Education
-          state={state.education}
-          onChange={onWorkEducationChange}
-          onAdd={onAdd}
-          onDelete={onDelete}
-        />
-        <Work
-          state={state.work}
-          onChange={onWorkEducationChange}
-          onAdd={onAdd}
-          onDelete={onDelete}
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form>
+      <Personal state={state.personal} onChange={onPersonalChange} />
+      <Education
+        state={state.education}
+        onChange={onWorkEducationChange}
+        onAdd={onAdd}
+        onDelete={onDelete}
+      />
+      <Work
+        state={state.work}
+        onChange={onWorkEducationChange}
+        onAdd={onAdd}
+        onDelete={onDelete}
+      />
+    </form>
+  );
+};
 
 export default Form;
